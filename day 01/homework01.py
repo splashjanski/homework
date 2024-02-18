41504033456ea371a3afc9e3afc838ba524732e1
splashjanski <splashjanski@gmail.com>
2024-02-18 11:41:47 +0400

    day 01

--- day 01/homework01.py
+++ day 01/homework01.py
@@ -1,6 +1,11 @@
 from turtle import*
 
 
+
+speed(4.8)
+width(5)
+
+
 begin_fill()
 color("green")
 forward(200)
@@ -26,6 +31,31 @@ color("blue")
 
 forward(120)
 right(90)
+
+forward(60)
+right(90)
+
+
+forward(120)
+left(90)
+end_fill()
+
+penup()
+goto(200, 200)
+pendown()
+
+
+begin_fill()
+color("purple")
+
+left(120)
+forward(200)
+
+
+left(120)
+forward(200)
+end_fill()
+
 
 
 
